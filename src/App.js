2cfdc3eea187f2e7e67aca65d112944a72eda053
Jolt64 <jonathantelford64@gmail.com>
2022-04-22 15:19:52 -0500

    Repo Starting Point. Words hardcoded buttons working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useReducer } from "react";
+
 
 function App() {
+
+  const [state, newState] = useReducer(
+    (state, newState) => ({...state, ...newState}),
+  {noun1: ' "Select a button" ', adjective1: ' "Select a button" '}
+);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <p className="Header">'This is your mvp'</p>
       </header>
+        <div className="TextMain">
+          El {state.noun1} es {state.adjective1}
+        </div>
+        <div>
+        <button className="ButtonWordChange"
+          onClick={() => {
+            state = newState({noun1: NounPicker()})
+          }}
+        >
+          Noun Click Me!
+        </button>
+        <button className="ButtonWordChange"
+          onClick={() => {
+            state = newState({adjective1: AdjectivePicker()})
+          }}
+        >
+          Adjective Click Me!
+        </button>
+        <button className="ButtonWordChange"
+          onClick={() => {
+            state = newState({noun1: NounPicker(), adjective1: AdjectivePicker()})
+          }}
+          >
+          Both Click Me!
+        </button>
+          </div>
     </div>
   );
+  function NounPicker() {
+    return NounArray[Math.floor(Math.random() * NounArray.length)];
+  }
+  
+  function AdjectivePicker() {
+    return AdjectiveArray[Math.floor(Math.random() * AdjectiveArray.length)];
+  }
 }
+
+let AdjectiveArray = ["rojo", "rápido", "verde"];
+let NounArray = ["Gato", "Hombre", "Perro"];
+
 
 export default App;
